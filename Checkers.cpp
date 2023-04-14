c7140d9717ecb9dc0d7d73c3519033326d468e3f
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-04-14 13:55:39 +0300

    4 added logic of checking for steps, and overload of function "Player"

--- Checkers.cpp
+++ Checkers.cpp
@@ -1,4 +1,5 @@
 #include<iostream>;
+#include<cstdint>;
 #include<Windows.h>;
 using namespace std;
 
@@ -17,7 +18,13 @@ void fillDesk(char desk[8][8])
 {
 	for (int i = 0; i < 8; i++)
 		for (int j = 0; j < 8; j++)
-			desk[i][j] = '-';
+		{
+			if ((i + j) % 2)
+				desk[i][j] = '#';
+			else
+				desk[i][j] = '-';
+		}
+			
 
 	for (int i = 0; i < 8; i++)
 	{
@@ -36,33 +43,49 @@ void fillDesk(char desk[8][8])
 	}
 }
 
-bool CheckStepPlayer1(char desk[8][8], int &x, int &y, int x1, int y1, int x2, int y2)
+bool CheckStepPlayer(char desk[8][8], int &x, int &y, int x1, int y1, int x2, int y2, char ch)
 {
-	if (desk[y2][x2] == '-' && (abs(x2 - x1) == 1 && abs(y2 - y1) == 1))
+	if (desk[y2][x2] == '#' && (abs(x2 - x1) == 1 && abs(y2 - y1) == 1))
 	{
-			return true;
+		if (ch == 'X')
+		{
+			if (y2 < y1)
+				return true;
+		}
+		else
+			if (y2 > y1)
+				return true;
 	}
-	
-	if (desk[y2][x2] == '-' && (abs(x2-x1) > 1 && abs(y2-y1) > 1))
+
+	if (desk[y2][x2] == '#' && (abs(x2-x1) > 1 && abs(y2-y1) > 1))
 	{
 		x = (x1 + x2) / 2;
 		y = (y1 + y2) / 2;
 		
-		if (desk[y][x] == 'Y')
-			return true;
+		if (ch == 'X')
+		{
+			if (desk[y][x] == 'Y')
+				return true;
+		}
+		else
+			if (desk[y][x] == 'X')
+				return true;
 	}
+
+	return false;
 }
 
-void Player1(char desk[8][8])
+void Player(char desk[8][8], char ch)
 {
 	int x = 0, y = 0;
-	int x1 = 0, y1 = 0;
-	int x2 = 0, y2 = 0;
-	int count = 0;
+	int x1, y1;
+	int x2, y2;
 	bool tf = false;
 
+start1:
+	x1 = 0, y1 = 0;
 	cout 
-		<< "\nPlayer1:"
+		<< "\nPlayer " << ch << ": "
 		<< "\n  select pos \"Checker\":\n";
 	cout << "\t\tLetter: ";
 	cin >> checker1.x;
@@ -71,11 +94,18 @@ void Player1(char desk[8][8])
 
 	x1 = static_cast<int>(checker1.x) - 97;
 	y1 = abs(checker1.y -= 8);
-	desk[y1][x1] = '-';
+	if(desk[y1][x1] == ch)
+		desk[y1][x1] = '#';
+	else
+	{
+		cout << "\n\tNo step";
+		goto start1;
+	}
 
-	start1:
+start2:
+	x2 = 0, y2 = 0;
 	cout
-		<< "\nPlayer1:"
+		<< "\nPlayer " << ch << ": "
 		<< "\n  select new pos \"Checker\":\n";
 	cout << "\t\tLetter: ";
 	cin >> checker1.x;
@@ -85,52 +115,27 @@ void Player1(char desk[8][8])
 	x2 = static_cast<int>(checker1.x) - 97;
 	y2 = abs(checker1.y -= 8);
 
-	tf = CheckStepPlayer1(desk, x, y, x1, y1, x2, y2);
+	if (desk[y2][x2] != '#')
+	{
+		cout << "\n\tNo step";
+		goto start2;
+	}
+
+	tf = CheckStepPlayer(desk, x, y, x1, y1, x2, y2, ch);
 	if (tf)
 	{
-		desk[y2][x2] = 'X';
+		desk[y2][x2] = ch;
 		if (abs(x2 - x1) > 1 && abs(y2 - y1) > 1)
 		{
-			desk[y][x] = '-';
-			count++;
+			desk[y][x] = '#';
 		}
 	}
 	else
 	{
 		cout << "\n\tNo step";
-		goto start1;
+		goto start2;
 	}
 	
-}
-
-void Player2(char desk[8][8])
-{
-	int x1 = 0, y1 = 0;
-	int x2 = 0, y2 = 0;
-
-	cout
-		<< "\nPlayer2:"
-		<< "\n  select pos \"Checker\":\n";
-	cout << "\t\tLetter: ";
-	cin >> checker2.x;
-	cout << "\t\tNumber: ";
-	cin >> checker2.y;
-
-	x1 = static_cast<int>(checker2.x) - 97;
-	y1 = abs(checker2.y -= 8);
-	desk[y1][x1] = '-';
-
-	cout
-		<< "\nPlayer2:"
-		<< "\n  select new pos \"Checker\":\n";
-	cout << "\t\tLetter: ";
-	cin >> checker2.x;
-	cout << "\t\tNumber: ";
-	cin >> checker2.y;
-
-	x2 = static_cast<int>(checker2.x) - 97;
-	y2 = abs(checker2.y -= 8);
-	desk[y2][x2] = 'Y';
 }
 
 void dataProcess(char desk[8][8])
@@ -141,11 +146,12 @@ void dataProcess(char desk[8][8])
 	
 	do
 	{
-		Player1(desk);
+		
+		Player(desk, 'X');
 		system("CLS");
 		dataPrint(desk);
 
-		Player2(desk);
+		Player(desk, 'Y');
 		system("CLS");
 		dataPrint(desk);
 
@@ -161,13 +167,13 @@ void dataPrint(char desk[8][8])
 	cout << endl;
 	for (int i = 0; i < 8; i++)
 	{
-		cout << " " << 8 - i << " | ";
+		cout << "\t" << 8 - i << " | ";
 		for (int j = 0; j < 8; j++)
 			cout << desk[i][j] << " | ";
 		cout << endl << " ";
 		if (i < 8)
 		{
-			cout << "  ";
+			cout << "\t  ";
 			for (int j = 0; j < 8; j++)
 				cout << " ---";
 		}
@@ -177,7 +183,7 @@ void dataPrint(char desk[8][8])
 	for (int i = 0; i < 8; i++)
 	{
 		if (i == 0)
-			cout << "\n     ";
+			cout << "\n\t    ";
 		cout << ch++ << "   ";
 	}
 	cout << endl;
