b8267d3bb9974d0279a6511b6c3c1b5e3ec325bd
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-04-14 10:33:03 +0300

    3 Added logic of step to kick black checker

--- Checkers.cpp
+++ Checkers.cpp
@@ -36,34 +36,71 @@ void fillDesk(char desk[8][8])
 	}
 }
 
+bool CheckStepPlayer1(char desk[8][8], int &x, int &y, int x1, int y1, int x2, int y2)
+{
+	if (desk[y2][x2] == '-' && (abs(x2 - x1) == 1 && abs(y2 - y1) == 1))
+	{
+			return true;
+	}
+	
+	if (desk[y2][x2] == '-' && (abs(x2-x1) > 1 && abs(y2-y1) > 1))
+	{
+		x = (x1 + x2) / 2;
+		y = (y1 + y2) / 2;
+		
+		if (desk[y][x] == 'Y')
+			return true;
+	}
+}
+
 void Player1(char desk[8][8])
 {
+	int x = 0, y = 0;
 	int x1 = 0, y1 = 0;
 	int x2 = 0, y2 = 0;
+	int count = 0;
+	bool tf = false;
 
 	cout 
 		<< "\nPlayer1:"
-		<< "\n select pos \"Checker\":\n";
-	cout << "\tLetter: ";
+		<< "\n  select pos \"Checker\":\n";
+	cout << "\t\tLetter: ";
 	cin >> checker1.x;
-	cout << "\tNumber: ";
+	cout << "\t\tNumber: ";
 	cin >> checker1.y;
 
-	x1 = static_cast<int>(checker1.x) - 65;
+	x1 = static_cast<int>(checker1.x) - 97;
 	y1 = abs(checker1.y -= 8);
 	desk[y1][x1] = '-';
 
+	start1:
 	cout
 		<< "\nPlayer1:"
-		<< "\n select new pos \"Checker\":\n";
-	cout << "\tLetter: ";
+		<< "\n  select new pos \"Checker\":\n";
+	cout << "\t\tLetter: ";
 	cin >> checker1.x;
-	cout << "\tNumber: ";
+	cout << "\t\tNumber: ";
 	cin >> checker1.y;
 
-	x2 = static_cast<int>(checker1.x) - 65;
+	x2 = static_cast<int>(checker1.x) - 97;
 	y2 = abs(checker1.y -= 8);
-	desk[y2][x2] = 'X';
+
+	tf = CheckStepPlayer1(desk, x, y, x1, y1, x2, y2);
+	if (tf)
+	{
+		desk[y2][x2] = 'X';
+		if (abs(x2 - x1) > 1 && abs(y2 - y1) > 1)
+		{
+			desk[y][x] = '-';
+			count++;
+		}
+	}
+	else
+	{
+		cout << "\n\tNo step";
+		goto start1;
+	}
+	
 }
 
 void Player2(char desk[8][8])
@@ -73,25 +110,25 @@ void Player2(char desk[8][8])
 
 	cout
 		<< "\nPlayer2:"
-		<< "\n select pos \"Checker\":\n";
-	cout << "\tLetter: ";
+		<< "\n  select pos \"Checker\":\n";
+	cout << "\t\tLetter: ";
 	cin >> checker2.x;
-	cout << "\tNumber: ";
+	cout << "\t\tNumber: ";
 	cin >> checker2.y;
 
-	x1 = static_cast<int>(checker2.x) - 65;
+	x1 = static_cast<int>(checker2.x) - 97;
 	y1 = abs(checker2.y -= 8);
 	desk[y1][x1] = '-';
 
 	cout
 		<< "\nPlayer2:"
-		<< "\n select new pos \"Checker\":\n";
-	cout << "\tLetter: ";
+		<< "\n  select new pos \"Checker\":\n";
+	cout << "\t\tLetter: ";
 	cin >> checker2.x;
-	cout << "\tNumber: ";
+	cout << "\t\tNumber: ";
 	cin >> checker2.y;
 
-	x2 = static_cast<int>(checker2.x) - 65;
+	x2 = static_cast<int>(checker2.x) - 97;
 	y2 = abs(checker2.y -= 8);
 	desk[y2][x2] = 'Y';
 }
@@ -128,18 +165,19 @@ void dataPrint(char desk[8][8])
 		for (int j = 0; j < 8; j++)
 			cout << desk[i][j] << " | ";
 		cout << endl << " ";
-		if (i < 7)
+		if (i < 8)
 		{
 			cout << "  ";
 			for (int j = 0; j < 8; j++)
 				cout << " ---";
 		}
+		if (i < 7)
 		cout << endl;
 	}
 	for (int i = 0; i < 8; i++)
 	{
 		if (i == 0)
-			cout << "col  ";
+			cout << "\n     ";
 		cout << ch++ << "   ";
 	}
 	cout << endl;
